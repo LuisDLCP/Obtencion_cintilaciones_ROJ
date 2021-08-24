d9689768153e8e82cdc97ecd1429298077749731
luis <luis11.dlcp@gmail.com>
2021-08-24 17:25:58 -0500

    ismr (LISN format) script was created

--- src/ismr2S4-LISN.py
+++ src/ismr2S4-LISN.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3.6
-# This script gets s4 files in the modified LISN format 
+# This script gets s4 files in the LISN format 
 # Author: Luis De La Cruz
 import numpy as np
 import pandas as pd
@@ -10,80 +10,73 @@ import os
 root_path = "/home/cesar/Desktop/luisd/scripts/Obtencion_cintilaciones/"
 input_files_path = root_path + "data_input/Data_set/"
 input_files_path_op = root_path + "data_input/Data_procesada/"
-output_files_path = root_path + "data_output/lisn/"
+output_files_path = root_path + "data_output/lisn-enriched/"
 
 # Read ISMR files
 def readISMR(input_file_name):
     # Read ismr file
-    data = pd.read_csv(input_files_path + input_file_name, header=None, squeeze=True)
+    data = pd.read_csv(input_files_path + input_file_name, 
+                        usecols=[0,1,2,4,5,7,8], header=None, squeeze=True)
     return data
 
 # Convert an ISMR to LISN format file
 def ismr2lisn(data):
-    # Extract data
-    WN = data[0] # Week number, since first epoch
-    TOW = data[1] # Time of week (s)
-    SVN = data[2]
+    # Filter only the GPS constellation 
+    mask1 = data[2].astype('int')<38 # SVID=PRN
+    mask2 = data[2].astype('int')>0
+    mask = mask1 & mask2 
+    data = data[mask]
 
-    # Create the HEADER columns as lists
-    yr = [] # year
-    dyr = [] # day_year
-    sd = [] # seconds_day
-    prn = [] # prn
-    for i in range(len(WN)):
-        fecha = WeekSeconds2UTC(int(WN[i]), int(TOW[i]), 0)
-        yr.append(fecha["year"])
-        dyr.append(fecha["day_year"])
-        sd.append(fecha["seconds_day"])
-        prn.append(get_PRN(int(SVN[i])))
+    # Get YY, DOY, SOD
+    data[["YY","DOY","SOD"]] = data[[0,1]].astype('float').apply(WeekSeconds2UTC, axis=1)
+    del data[0]
+    del data[1]
 
-    # Create the HEADER dataframe by joining the column lists
-    list_tuples = list(zip(yr,dyr,sd,prn)) # creates a list of tuples
-    dataFrame = pd.DataFrame(list_tuples, columns=['year','day','seconds','PRN']) # creates a pandas dataframe
+    # Rename columns
+    data.rename(columns={2:"PRN", 4: "Azim", 5:"Elev"}, inplace=True)
 
-    # Adding other pandas series(BODY)
-    dataFrame['alpha'] = data[4]
-    dataFrame['epsilon'] = data[5]
-    dataFrame['S4_sig1'] = data[7] # S4 on "Sig1"
-    dataFrame['S4corr_sig1'] = data[8] # Correction to S4 on Sig1
-    dataFrame['S4_sig2'] = data[32] # S4 on "Sig2"
-    dataFrame['S4corr_sig2'] = data[33] # Correction to S4 on Sig2
-    dataFrame['S4_sig3'] = data[46] # S4 on "Sig3"
-    dataFrame['S4corr_sig3'] = data[47] # Correction to S4 on Sig3
-    dataFrame['phi1_sig1'] = data[9] # Phi 01 on Sig1
-    dataFrame['phi3_sig1'] = data[10] # Phi 03 on Sig1
-    dataFrame['phi10_sig1'] = data[11] # Phi 10 on Sig1
-    dataFrame['phi30_sig1'] = data[12] # Phi 30 on Sig1
-    dataFrame['phi60_sig1'] = data[13] # Phi 60 on Sig1
-    dataFrame['phi1_sig2'] = data[34] # Phi 01 on Sig2
-    dataFrame['phi3_sig2'] = data[35] # Phi 03 on Sig2
-    dataFrame['phi10_sig2'] = data[36] # Phi 10 on Sig2
-    dataFrame['phi30_sig2'] = data[37] # Phi 30 on Sig2
-    dataFrame['phi60_sig2'] = data[38] # Phi 60 on Sig2
-    dataFrame['phi1_sig3'] = data[48] # Phi 01 on Sig3
-    dataFrame['phi3_sig3'] = data[49] # Phi 03 on Sig3
-    dataFrame['phi10_sig3'] = data[50] # Phi 10 on Sig3
-    dataFrame['phi30_sig3'] = data[51] # Phi 30 on Sig3
-    dataFrame['phi60_sig3'] = data[52] # Phi 60 on Sig3
+    # Calculate the corrected S4
+    def get_correctedS4(row):
+        s4 = row.iloc[0]
+        correction= row.iloc[1]
+        # Treat nan numbers 
+        if pd.isnull(s4) or pd.isnull(correction):
+            return np.nan
+        else:
+            # Calculate the corrected S4
+            x = s4**2-correction**2
+            if x>0:
+                return x**0.5
+            else:
+                return 0
+    
+    data['S4'] = data[[7,8]].astype("float").apply(get_correctedS4, axis=1)
+    del data[7]
+    del data[8]
+
+    # Rearrange columns 
+    dataFrame = data[["YY","DOY","SOD","PRN","S4","Azim","Elev"]]
 
     # Create the final dataFrame
     def create_rowDataFrame(seconds):
         # Select a subset of data
-        subset1 = dataFrame[dataFrame["seconds"] == seconds]
+        subset1 = dataFrame[dataFrame["SOD"] == seconds]
         # Create the list header
-        header = list(subset1.iloc[0,:3])
+        header = list(subset1.iloc[0,:3].astype("int"))
         header.append(len(subset1))
         # Create the list body
         ss1 = subset1.loc[:,"PRN":] # Select some columns
         # Merge the list header and body
         for i in range(len(ss1)):
-            header += list(ss1.iloc[i])
+            body = list(ss1.iloc[i]) # float values
+            body = [int(body[0])]+body[1:] # change PRN datatype: float -> int 
+            header += body
         # Create the row dataframe
         parameters = pd.DataFrame([header])
 
         return parameters
 
-    seconds_list = dataFrame['seconds']
+    seconds_list = dataFrame['SOD']
     seconds_list = list(seconds_list.drop_duplicates())
 
     for i in range(len(seconds_list)):
@@ -97,7 +90,11 @@ def ismr2lisn(data):
 
 # Convert GPS time: week & seconds; to UTC time.
 # OJO: It's missing the leapseconds, these value get from the navigation file
-def WeekSeconds2UTC(gpsweek,gpsseconds,leapseconds):
+def WeekSeconds2UTC(row):
+    gpsweek = row[0]
+    gpsseconds = row[1]
+    leapseconds = 0 # specify in the argument if it is given 
+
     datetimeformat = "%Y-%m-%d %H:%M:%S"
     first_epoch = datetime.datetime.strptime("1980-01-06 00:00:00",datetimeformat)
     elapsed = datetime.timedelta(days=(gpsweek*7),seconds=(gpsseconds-leapseconds))
@@ -112,7 +109,7 @@ def WeekSeconds2UTC(gpsweek,gpsseconds,leapseconds):
     second = int(date[13:15])
     seconds_day = second*1 + minute*60 + hour*60*60
 
-    return {"year":year, "day_year":day_year, "seconds_day": seconds_day}
+    return pd.Series({'YY':year, 'DOY':day_year, 'SOD':seconds_day})
 
 # Convert SVIDs to PRN codes. The PRNs code were obtained from PolaRx5S Reference Guide.
 def get_PRN(svid):
@@ -146,12 +143,13 @@ def get_PRN(svid):
     return prn
 
 # Generate lisn file name
-def get_file_name(file_name, data):
-    station_name = file_name[:4]
+def get_file_name(file_name): # filename: ljic2710.20_.ismr
+    station_name = file_name[:4] # ljic
 
-    a = list(data.iloc[0,[0,1]])
-    today = str(a[0])+"-"+str(a[1])+";00:00:00" # e.g. '20-219;00:00:00'
-    fecha = datetime.datetime.strptime(today,"%y-%j;%H:%M:%S")
+    YY = file_name[-8:-6] # 20
+    DOY = file_name[4:7] # 271
+    today = YY + "-" + DOY # e.g. '21-271'
+    fecha = datetime.datetime.strptime(today,"%y-%j")
     fecha = datetime.datetime.strftime(fecha,"%y-%m-%d") #e.g. '20-08-06'
     year = fecha[:2]
     month = fecha[3:5]
@@ -162,7 +160,7 @@ def get_file_name(file_name, data):
     return file_name
 
 def save_csv(file_name, value):
-    s4_name = get_file_name(file_name, value)
+    s4_name = get_file_name(file_name)
     # Save dataFrame to csv file
     value.to_csv(output_files_path + s4_name, sep='\t',index=False,header=False,encoding='utf-8')
     return "Ok"
@@ -180,4 +178,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-    print("Finish succesfully!")
+    print("Finished succesfully!")
--- src/readme.txt
+++ src/readme.txt
@@ -1,10 +1,12 @@
 Readme
 -------
 What's about?
-This script extracts scintillations info (S4 & sigma_phi) from an ISMR file.  
+This script extracts scintillations info (S4 & sigma_phi) from an ISMR file. Then, data is
+arranged in the LISN or modified LISN format.  
 -------
 Instructions:
 
 1) Place the ismr data into "~/data_input/Data_set/" folder
-2) Run ismr2S4.py
-2) The output data: scintillation file(*.s4), will be stored in "~/data_output" folder
+2) Run ismr2S4.py (modified LISN format)
+2) Run ismr2S4-LISN.py (LISN format)
+3) The output data: scintillation file(*.s4), will be stored in "~/data_output" folder
